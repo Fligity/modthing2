f14dc79fa284b637f2409a1a6e35b9312b0312b0
Fligity <callu@DESKTOP-1TQH392>
2021-11-11 01:38:29 +0000

    Pubes

---
+++ src/main/java/net/mcreator/biomesohplanty/item/PubeItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.biomesohplanty.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.biomesohplanty.BiomesOhPlantyModElements;
+
+@BiomesOhPlantyModElements.ModElement.Tag
+public class PubeItem extends BiomesOhPlantyModElements.ModElement {
+	@ObjectHolder("biomes_oh_planty:pube")
+	public static final Item block = null;
+	public PubeItem(BiomesOhPlantyModElements instance) {
+		super(instance, 88);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("pube");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
