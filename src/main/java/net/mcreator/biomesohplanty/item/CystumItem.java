8da5f75c6c4f20e7c90672a926383a47ed3e24e2
Fligity <callu@DESKTOP-1TQH392>
2021-12-02 00:23:24 +0000

    cystum

---
+++ src/main/java/net/mcreator/biomesohplanty/item/CystumItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.biomesohplanty.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.biomesohplanty.BiomesOhPlantyModElements;
+
+@BiomesOhPlantyModElements.ModElement.Tag
+public class CystumItem extends BiomesOhPlantyModElements.ModElement {
+	@ObjectHolder("biomes_oh_planty:cystum")
+	public static final Item block = null;
+	public CystumItem(BiomesOhPlantyModElements instance) {
+		super(instance, 105);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("cystum");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
