35655e21f1af2022f870acc76b3688206b9b4226
flioghty@outlook.com <tsilv@TOM.home>
2021-12-05 12:22:30 +0000

    abcon

--- src/main/java/net/mcreator/biomesohplanty/BiomesOhPlantyModElements.java
+++ src/main/java/net/mcreator/biomesohplanty/BiomesOhPlantyModElements.java
@@ -51,6 +51,8 @@ public class BiomesOhPlantyModElements {
 		sounds.put(new ResourceLocation("biomes_oh_planty", "calamity"),
 				new net.minecraft.util.SoundEvent(new ResourceLocation("biomes_oh_planty", "calamity")));
 		sounds.put(new ResourceLocation("biomes_oh_planty", "no"), new net.minecraft.util.SoundEvent(new ResourceLocation("biomes_oh_planty", "no")));
+		sounds.put(new ResourceLocation("biomes_oh_planty", "bacon"),
+				new net.minecraft.util.SoundEvent(new ResourceLocation("biomes_oh_planty", "bacon")));
 		try {
 			ModFileScanData modFileInfo = ModList.get().getModFileById("biomes_oh_planty").getFile().getScanResult();
 			Set<ModFileScanData.AnnotationData> annotations = modFileInfo.getAnnotations();
