2743d0dab3f88bcdb69642f034642f8f7d765444
flioghty@outlook.com <tsilv@TOM.home>
2021-12-05 12:21:03 +0000

    eee

--- src/main/java/net/mcreator/biomesohplanty/procedures/CaveProcedure.java
+++ src/main/java/net/mcreator/biomesohplanty/procedures/CaveProcedure.java
@@ -1,7 +1,24 @@
 package net.mcreator.biomesohplanty.procedures;
 
-public class CaveProcedure {
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.biomesohplanty.BiomesOhPlantyModVariables;
+import net.mcreator.biomesohplanty.BiomesOhPlantyMod;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class CaveProcedure {
 	@Mod.EventBusSubscriber
 	private static class GlobalTrigger {
 		@SubscribeEvent
@@ -18,7 +35,6 @@ public class CaveProcedure {
 			executeProcedure(dependencies);
 		}
 	}
-
 	public static void executeProcedure(Map<String, Object> dependencies) {
 		if (dependencies.get("x") == null) {
 			if (!dependencies.containsKey("x"))
@@ -40,12 +56,10 @@ public class CaveProcedure {
 				BiomesOhPlantyMod.LOGGER.warn("Failed to load dependency world for procedure Cave!");
 			return;
 		}
-
 		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
-
 		double test = 0;
 		BiomesOhPlantyModVariables.WorldVariables.get(world).test = (double) Math.random();
 		BiomesOhPlantyModVariables.WorldVariables.get(world).syncData(world);
@@ -63,5 +77,4 @@ public class CaveProcedure {
 			System.out.println("HOLY SHIT BRO");
 		}
 	}
-
 }
--- src/main/java/net/mcreator/biomesohplanty/procedures/GunBulletHitsPlayerProcedure.java
+++ src/main/java/net/mcreator/biomesohplanty/procedures/GunBulletHitsPlayerProcedure.java
@@ -1,7 +1,20 @@
 package net.mcreator.biomesohplanty.procedures;
 
-public class GunBulletHitsPlayerProcedure {
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 
+import net.minecraft.world.World;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.biomesohplanty.BiomesOhPlantyMod;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class GunBulletHitsPlayerProcedure {
 	@Mod.EventBusSubscriber
 	private static class GlobalTrigger {
 		@SubscribeEvent
@@ -27,30 +40,24 @@ public class GunBulletHitsPlayerProcedure {
 			}
 		}
 	}
-
 	public static void executeProcedure(Map<String, Object> dependencies) {
 		if (dependencies.get("entity") == null) {
 			if (!dependencies.containsKey("entity"))
 				BiomesOhPlantyMod.LOGGER.warn("Failed to load dependency entity for procedure GunBulletHitsPlayer!");
 			return;
 		}
-
 		Entity entity = (Entity) dependencies.get("entity");
-
 		double PlayerYaw = 0;
 		PlayerYaw = (double) (entity.rotationYaw);
 		PlayerYaw = (double) (PlayerYaw + 180);
 		entity.rotationYaw = (float) (PlayerYaw);
 		entity.setRenderYawOffset(entity.rotationYaw);
 		entity.prevRotationYaw = entity.rotationYaw;
-
 		if (entity instanceof LivingEntity) {
 			((LivingEntity) entity).prevRenderYawOffset = entity.rotationYaw;
 			((LivingEntity) entity).rotationYawHead = entity.rotationYaw;
 			((LivingEntity) entity).prevRotationYawHead = entity.rotationYaw;
 		}
-
 		entity.rotationPitch = (float) (0);
 	}
-
 }
